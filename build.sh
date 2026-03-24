ea087358654f32505c5befe48d2c40639ca2f6e8
Leonel Soto <54054109+Orinchan@users.noreply.github.com>
2026-03-23 22:56:20 -0600

    Add files via upload

--- build.sh
+++ build.sh
@@ -10,11 +10,4 @@ python manage.py collectstatic --no-input
 echo "Running migrations..."
 python manage.py migrate --verbosity 3
 
-echo "Loading initial data..."
-if [ -f "backup.json" ]; then
-    python manage.py loaddata backup.json
-else
-    echo "No backup.json found, skipping data load."
-fi
-
 echo "Build completed successfully!"
