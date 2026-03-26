07dc9715b0ac5992f0714b3185ba3f800e2448df
Leonel Soto <54054109+Orinchan@users.noreply.github.com>
2026-03-26 13:12:28 -0600

    Add files via upload

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ pip install -r requirements.txt
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
+echo "Running migrations..."
+python manage.py migrate
 
 echo "Build completed successfully!"
