a3313918f0f71af8bc3ded5da89a54b93c4ad58e
Leonel Soto <54054109+Orinchan@users.noreply.github.com>
2026-03-26 13:04:38 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,5 @@ pip install -r requirements.txt
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
-echo "Running migrations..."
-python manage.py migrate --verbosity 3
 
 echo "Build completed successfully!"
